85d8c78bb7831294e7c8c2d067a4a801ca9b9e5a
nsethupathi <nivethan.sethupathi@gmail.com>
2023-05-18 11:42:21 -0500

    first commit

---
+++ src/aws/service/ScalityService.go
@@ -0,0 +1,41 @@
+package service
+
+import (
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"os"
+	"time"
+)
+
+func exitErrorf(msg string, args ...interface{}) {
+	fmt.Fprintf(os.Stderr, msg+"\n", args...)
+	os.Exit(1)
+}
+
+func GetPresignedUrl(c *gin.Context) {
+	songName := c.Param("songName")
+
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	// Create S3 service client
+	svc := s3.New(sess)
+
+	songKey := fmt.Sprintf("music/%s.mp3", songName)
+	req, _ := svc.GetObjectRequest(&s3.GetObjectInput{
+		Bucket: aws.String("niv-homepage-bucket"),
+		Key:    aws.String(songKey),
+	})
+	urlStr, err := req.Presign(15 * time.Minute)
+
+	if err != nil {
+		exitErrorf("Failed to sign request", err)
+	}
+
+	c.IndentedJSON(http.StatusOK, urlStr)
+}
