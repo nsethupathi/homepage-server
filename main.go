a110c348a9e221a10c61ce8f529d11063cbf9182
Nivethan Sethupathi <nivethan.sethupathi@gmail.com>
2023-10-15 19:53:09 -0500

    enables cors

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"homepage-server/src/orchestrator"
 	"homepage-server/src/showrepository"
@@ -11,6 +12,7 @@ func main() {
 	showRunner := showrepository.BuildRunner("SHOWS")
 
 	router := gin.Default()
+	router.Use(cors.Default())
 	router.GET("/shows", orchestrator.Orchestrate(showRunner))
 
 	router.Run("localhost:8080")
